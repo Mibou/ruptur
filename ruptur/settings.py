75b80647b323fb18f47f9fc8b927b2fbdfe99d02
Nicolas Pieuchot <n.pieuchot@zero-gachis.com>
2018-11-19 15:08:43 +0100

    Removing secret key.

--- ruptur/settings.py
+++ ruptur/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '69w=$9(otfr31()gybar9=!)1dyf5u=ntbh9igwwrv_!2r6v(6'
+SECRET_KEY = os.environ('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
