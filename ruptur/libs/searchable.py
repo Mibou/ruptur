045aedf748294e745208fa1d4068ebd83141123b
Nicolas Pieuchot <n.pieuchot@zero-gachis.com>
2018-12-28 09:25:17 +0100

    Changing search method to seach splitted.

--- ruptur/libs/searchable.py
+++ ruptur/libs/searchable.py
@@ -2,8 +2,30 @@ from django.db import models
 
 
 class Searchable(models.Model):
-    def search():
+
+    @classmethod
+    def search(cls, match):
         pass
+
+    @classmethod
+    def split_search(cls, match):
+        result = None
+        clean_match = match.strip()
+
+        if not clean_match:
+            return cls.objects.all()
+
+        for split in clean_match.split(' '):
+            if not split:
+                continue
+
+            if not result:
+                result = cls.search(split)
+                continue
+
+            result &= cls.search(split)
+
+        return result
 
     class Meta:
         abstract = True
