978c2ac5ce4159cc1b00cc270be9f58d26228832
Nicolas Pieuchot <n.pieuchot@zero-gachis.com>
2019-01-04 21:15:46 +0100

    Setting an auto update signal for Haystack

--- ruptur/settings.py
+++ ruptur/settings.py
@@ -29,6 +29,8 @@ HAYSTACK_CONNECTIONS = {
         'INDEX_NAME': 'haystack'
     }
 }
+HAYSTACK_SIGNAL_PROCESSOR = 'haystack.signals.RealtimeSignalProcessor'
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
